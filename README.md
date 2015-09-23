c107fb91aa3eee4e9d122404fc1a54a81ec779c4
xiangmingcai <hedong313@qq.com>
2015-09-23 17:04:26 +0800

    提交项目框架

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # chat
 毕业论文.聊天系统
+
+# 创建项目的时候使用ejs末班引擎
+express -e webchat
+
+
+
---
+++ models/login.js
@@ -0,0 +1,10 @@
+/**
+ * Created by xmc7276 on 2015/9/23.
+ */
+
+var login = function(req, res) {
+
+}
+
+module.exports = login;
+
