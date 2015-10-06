cf6f50da5a9dbfe7616d496f95c9d8eaa3229752
jamlake <hedong313@qq.com>
2015-10-06 17:30:07 +0800

    完成注册功能

--- routes/login.js
+++ routes/login.js
@@ -27,4 +27,39 @@ router.post('/check', function(req, res, next) {
     });
 
 });
+
+router.get('/reg', function(req, res, next) {
+
+    res.render('reg', {title: '注册' });
+
+});
+
+router.post('/doReg', function(req, res, next){
+
+    var name = req.param('name');
+    var password = req.param('password');
+    var sql = "select * from user where name='"+name+"'";
+    var C = new Conn();
+    var flag = false;
+
+    C.find(sql, function(err, ress){
+        if(ress[0] && ress[0].name) {
+            res.send({code:1, msg:'该用户名已经存在，请更换一个'});
+        } else {
+
+            var sql2 = "INSERT INTO `user`" +"(`name`,`password`,`add_time`) VALUES ('"+name+"','"+password+"',now())";
+
+            C.find(sql2, function(err2, ress2){
+                if(err2) {
+                    console.log(sql2);
+                    console.log(err2);
+                    res.send({code:0, msg: '注册失败'});
+                } else {
+                    res.send({code:0, msg: '注册成功,请登录'});
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
